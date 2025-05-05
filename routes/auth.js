57b8c5cef0adc9067c7da6dfc63a2407a43bc332
gavinball-UA <159508429+gavinball-UA@users.noreply.github.com>
2025-05-04 20:27:28 -0700

    Swap bcrypt for bcryptjs and update dependencies

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const User = mongoose.model('User');
 
 module.exports = function(passport) {
